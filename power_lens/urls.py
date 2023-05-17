bf4b2ad0d9c616dbd235c4fbac9532fe17bad723
João Pedro S. Albuquerque <jpedroabq@gmail.com>
2023-05-17 20:22:38 -0300

    Sistema de login adicionado

--- power_lens/urls.py
+++ power_lens/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  
 from apps.power_lens.views import homePage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homePage),
+    path('auth/', include('apps.power_lens.urls'))
 ]
