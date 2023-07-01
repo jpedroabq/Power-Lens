e54783aa8b6e5141e4995096ce579473a0eb1918
João Pedro S. Albuquerque <jpedroabq@gmail.com>
2023-07-01 09:35:04 -0300

    Divs invisíveis

--- static/js/grafico.js
+++ static/js/grafico.js
@@ -1,3 +1,5 @@
+//Torna a div do gráfico arrastável e redimensionável
+
 $(function () {
   $("#container").draggable({
     containment: "parent"
@@ -10,4 +12,23 @@ $(function () {
     handles: "n, e, s, w, ne, se, sw, nw"
   }
   );
+});
+
+
+let inputForm = document.getElementById('inputForm');
+
+inputForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let input = document.getElementById('input');
+
+  if (input.value ==""){
+    alert("Digite algo no campo de busca");
+  } else {
+    //torna a div visível
+    document.getElementById("container").style.visibility = "visible";
+
+    console.log(input.value);
+    input.value = "";
+  }
 });
