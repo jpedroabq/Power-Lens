98ea81f7ca6d19db4e60c1bc35a8800bb5c83fd1
Joao Felipe Nunes Carvalho <jfelipenc99@gmail.com>
2023-07-04 19:34:58 -0300

    Mudança nos ids selecionados e loop de divs

--- static/js/grafico.js
+++ static/js/grafico.js
@@ -1,13 +1,13 @@
 //Torna a div do gráfico arrastável e redimensionável
 
 $(function () {
-  $("#container").draggable({
+  $(".container").draggable({
     containment: "parent"
   });
 });
 
 $(function () {
-  $("#container").resizable({
+  $(".container").resizable({
     containment: "#parent",
     handles: "n, e, s, w, ne, se, sw, nw"
   }
@@ -15,20 +15,23 @@ $(function () {
 });
 
 
-let inputForm = document.getElementById('inputForm');
+let inputForm = document.getElementById('searchBtn');
+let i = 1;
 
-inputForm.addEventListener("submit", (e) => {
+inputForm.addEventListener("click", (e) => {
   e.preventDefault();
 
-  let input = document.getElementById('input');
+  let input = document.getElementById('searchBar');
 
   if (input.value ==""){
     alert("Digite algo no campo de busca");
   } else {
-    //torna a div visível
-    document.getElementById("container").style.visibility = "visible";
+    //torna a div # visível
+    let divName = "container" + i;
+    document.getElementById(divName).style.visibility = "visible";
 
     console.log(input.value);
     input.value = "";
+    i++;
   }
 });
