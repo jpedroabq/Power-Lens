876a9dbfbf088e29470dd0b46775c0938462949c
Joao Felipe Nunes Carvalho <jfelipenc99@gmail.com>
2023-08-28 13:23:12 -0300

    Retornando static_root e eliminando linha 138

--- power_lens/settings.py
+++ power_lens/settings.py
@@ -133,9 +133,9 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    #os.path.join(BASE_DIR, 'static')
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
